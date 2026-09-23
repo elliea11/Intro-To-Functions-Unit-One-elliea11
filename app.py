87c07c40d9da5aa0a307c55a07df169c439e5b7a
elliea11 <elliea11@nycstudents.net>
2026-09-23 08:46:58 -0400

    dfgh

--- app.py
+++ app.py
@@ -4,5 +4,54 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+""" t.forward(200) 
+def message(input):
+    print(input)
+message("Hello Class")
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)"""
+
+
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200)  """
+
+""" def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+""" def rectangle ():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle() """
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(90)
 turtle.done()
